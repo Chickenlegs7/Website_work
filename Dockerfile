0aff6deca21ca71ffef4da4f3b128256dd3bd3f1
Merrik Gilbert <merrik6@gmail.com>
2025-01-31 22:22:54 +0000

    working on getting website deployed

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.9
 
 ENV APP_HOME /Main
 WORKDIR $APP_HOME
+
 COPY . ./
 
 RUN pip install --no-cache-dir -r requirements.txt
--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,2 +1,2 @@
-gcloud builds submit --tag gcr.io/wildrose-industrial-services/railway1.0.0 .;
-gcloud run deploy --image=gcr.io/wildrose-industrial-services/railway1.0.0 --platform managed --region us-central1;
+gcloud builds submit --tag gcr.io/wildrose-industrial-services/railway:1.0.0 . --service-account 7289993003-compute@developer.gserviceaccount.com
+gcloud run deploy --image=gcr.io/wildrose-industrial-services/railway:1.0.0 --platform managed --region us-central1 --service-account 7289993003-compute@developer.gserviceaccount.com
